fb8461b3a2781e4554adafb43430f98b186f15ca
sttinh99 <17110381@student.hcmute.edu.vn>
2020-01-06 19:19:49 +0700

    update

--- 01.js
+++ 01.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded',function(){
     var nut = document.querySelectorAll('.thslide ul li')
     var slide = document.querySelectorAll('.phanslide ul li');
+    var nauD1 = document.querySelectorAll('.motslide .nauduoi1')
+    var nauD2 = document.querySelectorAll('.motslide .nauduoi2')
+    var nauT = document.querySelectorAll('.motslide .nautren')
+    var tnd = document.querySelectorAll('.motslide .textnoidung')
+    var vitribandau = 1;
     console.log(slide);
     
     for (let index = 0; index < nut.length; index++) {
@@ -13,4 +18,18 @@ document.addEventListener('DOMContentLoaded',function(){
             slide[index].classList.add('active');
         })
     }
+    var x = setInterval(function(){
+        var y = 5%vitribandau;
+        for (let index = 0; index < nut.length; index++) {
+            nut[index].classList.remove('macdinh');
+            slide[index].classList.remove('active');
+        }
+        nut[y].classList.add('macdinh');
+        slide[y].classList.add('active');
+        vitribandau++;
+        if(vitribandau==4)
+        {
+            vitribandau = 1;
+        }
+        },4000);
 },false);
